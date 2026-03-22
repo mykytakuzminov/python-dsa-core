11db22356a62b0f345e3ba822be9cb5c2415c421
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-03-22 17:12:10 +0100

    feat: migrate project to uv and restructure src layout
    * refactor: change tox configuration file
    * refactor: update project configuration file to use it with uv
    * refactor: reorganize project structure into src/dsa layout
    * refactor: remove docker file
    * refactor: reorganize project structure
    * feat: configure public API via __init__.py exports
    * fix: resolve circular imports and fix code style
    * chore: update .gitignore
    * fix: remove mistake in MinHeap name

---
+++ src/dsa/data_structures/__init__.py
@@ -0,0 +1,20 @@
+from .binary_search_tree import BinarySearchTree
+from .doubly_linked_list import DoublyLinkedList
+from .graph import Graph
+from .hash_map import HashMap
+from .heap import MaxHeap, MinHeap
+from .queue import Queue
+from .singly_linked_list import SinglyLinkedList
+from .stack import Stack
+
+__all__ = [
+    "BinarySearchTree",
+    "DoublyLinkedList",
+    "Graph",
+    "HashMap",
+    "MaxHeap",
+    "MinHeap",
+    "Queue",
+    "SinglyLinkedList",
+    "Stack",
+]
